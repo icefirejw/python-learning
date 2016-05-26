af8570b43f5b5e4426b0e15b2752b04a99edd59f
icefirejw <icefirejw@gmail.com>
2016-05-26 16:40:53 +0800

    day2
    learn using module

---
+++ day2/mymodule.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+#File mymodule.py
+#this is a module demo
+
+def max_num(a,b):
+    print "using demo module's function 'max_num'"
+    if (a>b):
+        return a
+    else:
+        return b
+
+def min_num(a,b):
+    print "using demo module's function 'min_num'"
+    if (a<b):
+        return a
+    else:
+        return b
+
+#end module demo
---
+++ day2/using_mymodule.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#File: using_mymodule.py
+#this is a using my module domo
+
+import mymodule
+
+print mymodule.max_num(3,4)
+print mymodule.min_num(3,4)
+
