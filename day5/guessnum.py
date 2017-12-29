16ca08b1e9db2a34664315d99154895a31f5e005
SamWang <icefirejw@gmail.com>
2017-12-29 12:38:45 +0800

    update the guess number program

--- day5/guessnum.py
+++ day5/guessnum.py
@@ -1,25 +1,30 @@
 import random
 
-def guessnum(num, count):
-    print('input the guess number');
-    for i in range(1,count,1):
-        guess = input()
+def guessnum(num, numrag, count):
+    print('guess a number between 1 and '+ str(numrag) + ', try ' + str(count) + ' times')
+    c = 0
+    while (c < count):
+        guess = int(input())
+        c = c + 1
         if (guess < num):
             print("too small")
+            continue
         elif (guess > num):
             print("too big")
+            continue
         else:
             break
 
-    if (i <= count):
+    if (c < count or (c==count and guess == num)):
         print('well done, clever boy!')
         return True;
     else:
-        print('keep trying');
-        return False;
+        print('keep trying')
+        return False
 
-number = random.randint(1,10)
-guessnum(number, 5)
+numrange = 60
+number = random.randint(1,numrange)
+guessnum(number, numrange, 5)
 
 
 
