10260794195ce860d0f1fe427f08505c1aede792
SamWang <icefirejw@gmail.com>
2018-10-20 18:51:15 +0800

    Update http_spider/http_spider.py

--- http_spider/http_spider.py
+++ http_spider/http_spider.py
@@ -1,16 +1,22 @@
+'''
+learning web spider
+'''
 import urllib.request
 
-def get_content(s):
-    url = s
+def get_content(url):
+    ''' get_content url '''
     req = urllib.request.Request(url)
     response = urllib.request.urlopen(req)
     print(response.info())
     print(response.getcode())
 
-def translation(s):
+def translation(docs):
+    '''  using youdao translation '''
+    data = {}
+    data[] = 
     req = urllib.request.Request();
+
 
 
 if __name__ == "__main__":
     get_content("http://placekitten.com/200/300")
-
