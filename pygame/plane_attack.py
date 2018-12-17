d531291dad9400e506293a7380417b76a8bf134e
SamWang <icefirejw@gmail.com>
2018-12-17 08:32:27 +0800

    plane_attack_v1.0
    finished the plane attacking version 1.0

--- pygame/plane_attack.py
+++ pygame/plane_attack.py
@@ -14,6 +14,10 @@ class PlaneGame(object):
         self.clock = pygame.time.Clock()
         # 定义图像精灵和精灵组
         self.__create_sprites()
+        # define the timer for enemy plane presenting
+        # present, each 1 secend
+        pygame.time.set_timer(CREATE_ENEMY_PLANE, 1000) 
+        pygame.time.set_timer(HERO_FIRE_EVENT, 500)
 
     def game_start(self):
         print("start game ...")
@@ -36,6 +40,13 @@ class PlaneGame(object):
         bg2 = Background(True)
         self.bg_sprites_group = pygame.sprite.Group(bg1,bg2)
 
+        # create the enemy sprites group 
+        self.enemy_group = pygame.sprite.Group()
+
+        # create the hero plane sprite and group
+        self.hero_plane = HeroPlaneSprite()
+        self.hero_plane_group = pygame.sprite.Group(self.hero_plane)
+
 
     def __event_handler(self):
         for event in pygame.event.get():
@@ -43,16 +54,54 @@ class PlaneGame(object):
             # 判断是否退出游戏
             if event.type == pygame.QUIT:
                 PlaneGame.__game_over()
+            elif event.type == CREATE_ENEMY_PLANE:
+                enemy = EnemySprite()
+                self.enemy_group.add(enemy)
+            elif event.type == HERO_FIRE_EVENT:
+                self.hero_plane.fire()
+            #elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+            #    print("to right")
+        
+        # get which key pressed by keyboard method 
+        key_pressed = pygame.key.get_pressed()
+        if (key_pressed[pygame.K_RIGHT]):
+            self.hero_plane.speed = 2
+        elif (key_pressed[pygame.K_LEFT]):
+            self.hero_plane.speed = -2
+        else:
+            self.hero_plane.speed = 0
+
 
     def __update_sprites(self):
         '''使用sprite模块,更新角色位置和信息'''
         self.bg_sprites_group.draw(self.screen)
         self.bg_sprites_group.update()
 
-        pass
+        # update & draw the enemy plane 
+        self.enemy_group.update()
+        self.enemy_group.draw(self.screen)
+        
+        # update & draw the hero plane
+        self.hero_plane_group.update()
+        self.hero_plane_group.draw(self.screen)
+
+        # update & draw the bullet
+        self.hero_plane.bullets.update()
+        self.hero_plane.bullets.draw(self.screen)
 
     def __check_collision(self):
         '''检查飞机碰撞'''
+        # 子弹打中了敌机 
+        pygame.sprite.groupcollide(self.hero_plane.bullets, self.enemy_group,True, True)
+
+        # 飞机碰到敌机 
+        col_list = pygame.sprite.spritecollide(self.hero_plane, self.enemy_group, True)
+
+        # 判断敌机是否碰到了飞机
+        if len(col_list) > 0:
+            self.hero_plane.kill()
+            PlaneGame.__game_over()
+
 
     @staticmethod
     def __game_over():
--- pygame/plane_sprite.py
+++ pygame/plane_sprite.py
@@ -5,11 +5,14 @@
  __init__(self, image_name, speed)
  update(self)
 '''
+import random
 import pygame
 
 # define the screen size
 SCREEN_REC = pygame.rect.Rect(0,0,480,700)
 FRAME_PER_SEC = 60
+CREATE_ENEMY_PLANE = pygame.USEREVENT
+HERO_FIRE_EVENT = pygame.USEREVENT + 1
 
 class GameSprite(pygame.sprite.Sprite):
     def __init__(self, image_name, speed = 1):
@@ -48,3 +51,69 @@ class Background(GameSprite):
         # 2. 判断图像是否移出了屏幕，如果移出了屏幕，则返回到屏幕的最上方
         if self.rect.y >= SCREEN_REC.height:
             self.rect.y = -self.rect.height
+
+class EnemySprite(GameSprite):
+    def __init__(self):
+        #调用父类init函数，加载enemy plane 图片
+        super().__init__('./plane_images/enemy1.png')
+        # define the enemy plane's speed
+        self.speed = random.randint(1,3)
+
+        # define the enemy plane's position
+        # the enemy's x is a random in the screen
+        self.rect.x = random.randint(0, SCREEN_REC.width-self.rect.width)
+
+    def update(self):
+        super().update()
+
+        # if the eneny is out of the screen, then kill itself.
+        if (self.rect.y > SCREEN_REC.bottom):
+            self.kill()
+
+    #def __del__(self):
+    #    print("deleting my self...")
+
+class HeroPlaneSprite(GameSprite):
+    '''define the hero plane'''
+    def __init__(self):
+        super().__init__("./plane_images/me1.png")
+
+        #define the hero's position 
+        self.rect.bottom = SCREEN_REC.bottom - 120
+        self.rect.centerx = SCREEN_REC.centerx
+
+        # define the bullet sprite group
+        self.bullets = pygame.sprite.Group()
+    
+    def update(self):
+        self.rect.x += self.speed
+
+        if (self.rect.left <= SCREEN_REC.left):
+            self.rect.left = SCREEN_REC.left
+        
+        if (self.rect.right >= SCREEN_REC.right):
+            self.rect.right = SCREEN_REC.right
+    
+    def fire(self):
+       
+        # fire 3 bullets for one time
+        for i in range(3):
+            # init the buttle sprint
+            buttle = BulletSprite()
+   
+            # init the buttle's position
+            buttle.rect.bottom = self.rect. y - 20*i
+            buttle.rect.centerx = self.rect.centerx
+            # add the buttle to sprite group 
+            self.bullets.add(buttle)
+
+class BulletSprite(GameSprite):
+    '''defined the bullet sprint '''
+    def __init__(self):
+        super().__init__('./plane_images/bullet1.png')
+        self.speed = -2
+    
+    def update(self):
+        super().update()
+        if self.rect.y < SCREEN_REC.top:
+            self.kill()
